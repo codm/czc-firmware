7fd560205c6f062979abb05c5f6e562311a44123
Tobias Schmitt <tschmittldk@gmail.com>
2024-04-05 12:40:36 +0200

    change version in files

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.2.0"
+        #define VERSION "0.0.2"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2024-01-14 18:40:00.000000"
